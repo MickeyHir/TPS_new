960b4838b7c8adac2360994e6d7daccd8a5c5ae8
Dmytro Tkachuk <dmytro.tkachuk.work@gmail.com>
2020-08-09 16:23:47 +0300

    movement scripts

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform target;
+    public const float mainOffset = 5;
+    public const float offsetY = 2;
+    public Vector3 offset = new Vector3();
+    public Vector3 offsetNew = new Vector3(0, offsetY, - mainOffset);
+    public float speed = 10;
+    public float smooth = 3;
+    public bool isFreeCamera = false;
+    bool isMouseKeyPresed = false;
+    Vector3 oldPos = new Vector2();
+    float maxdis = 10;
+    
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            isMouseKeyPresed = true;
+            oldPos = Input.mousePosition;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            isMouseKeyPresed = false;
+        }
+        if(!isFreeCamera)
+        {
+            offset.x = -Mathf.Sin(target.rotation.eulerAngles.y*Mathf.Deg2Rad) * mainOffset;
+            offset.y = offsetY;
+            offset.z = -Mathf.Cos(target.rotation.eulerAngles.y*Mathf.Deg2Rad) * mainOffset;
+        }
+        else
+        {
+            if(isMouseKeyPresed)
+            {
+                offsetNew.y +=MouseDelta().y * 0.0001f;
+            }
+            offset = offsetNew;
+        }
+        Vector3 look = target.position - this.transform.position;
+        this.transform.position = Vector3.Lerp(this.transform.position, target.position + offset, smooth*Time.deltaTime);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(look), speed * Time.deltaTime);
+    }
+
+    public Vector3 MouseDelta()
+    {
+        Vector3 delta = new Vector3();
+        /*if (Input.GetMouseButtonDown(0) && !isMouseKeyPresed)
+        {
+            isMouseKeyPresed = true;
+            oldPos = Input.mousePosition;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            isMouseKeyPresed = false;
+            delta = Input.mousePosition - oldPos;
+        }*/
+        delta = Input.mousePosition - oldPos;
+        return delta;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static float step = 10;
+    public float rotationSpeed;
+    public float jump;
+    private float _direction = 0;
+    private float _movement = step;
+    public float rotationAmount;
+    public MovementType movementType;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if(Input.GetKey("space"))
+            this.gameObject.GetComponent<Rigidbody>().AddForce(0,jump,0);
+
+        if(Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            switch(movementType)
+            {
+                case MovementType.CAMERA: CameraModel(); break;
+                case MovementType.RACE: RaceModel();  break;
+                default: break; 
+            }
+            Motion(_direction, _movement);
+        }
+        else
+        {
+            _direction = transform.eulerAngles.y;
+        }
+        /*if(!IsRotationFinished(transform.eulerAngles.y, _direction))
+        {
+            Motion(_direction, 0);
+            Debug.Log(transform.eulerAngles.y - _direction);
+        }*/
+    }
+
+    private void CameraModel()
+    {
+        float dir = 0f;
+        float count = 0;
+        if(Input.GetKey("a"))
+        {
+            count++;
+            dir = -90;
+        }
+        if(Input.GetKey("d"))
+        {
+            count++;
+            dir = 90;
+        }
+        if(Input.GetKey("w"))
+        {
+            count++;
+            dir = dir / 2;
+        }
+        if(Input.GetKey("s"))
+        {
+            count++;
+            dir = (180 * count)+(dir + dir/2);
+        }
+        _direction = dir;
+    }
+
+    private void RaceModel()
+    {
+        float forvard=0;
+        float dir = 0f;
+
+        if(Input.GetKey("a"))
+        {
+            dir -= rotationAmount;
+        }
+        if(Input.GetKey("d"))
+        {
+            dir += rotationAmount;
+        }
+        if(Input.GetKey("w"))
+        {
+            forvard = step;
+        }
+        if(Input.GetKey("s"))
+        {
+            forvard = -step;
+        }
+        _direction += dir;
+        _movement = forvard;
+    }
+
+    private void Motion(float rotation = 0f, float position = 0f) //rotation for dir, position for forvard amount
+    {
+        Quaternion lookdir = new Quaternion(); 
+        lookdir.eulerAngles = new Vector3(0,rotation,0);
+        this.transform.localRotation = Quaternion.Lerp(transform.rotation, lookdir, rotationSpeed*Time.deltaTime);
+        this.transform.Translate(new Vector3(0,0,position*Time.deltaTime), Space.Self);
+    }
+
+    private bool IsRotationFinished(float actualRotation, float destrotation) => Mathf.Abs(actualRotation - destrotation) < 5f ? true : false;
+
+    public enum MovementType
+    {
+        CAMERA,
+        RACE
+    }
+}
